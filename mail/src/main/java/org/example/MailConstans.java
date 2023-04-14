7e3ccba057ca1a52a273ea9d95189fdb2bfba703
nitesh choudhary <niteshchoudhary1217@gmail.com>
2023-04-14 17:59:01 +0530

    Initial commit

---
+++ mail/src/main/java/org/example/MailConstans.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class MailConstans {
+    public static final String SENDERMAIL = "niteshjaiswal7783@gmail.com";
+
+    // receiver email
+    public static final String RECEIVERMAIL = "niteshchoudhary1217@gmail.com";
+
+    //subject of the mail
+    public static final String SUBJECT = "Testing Java mail : 12-April-2023";
+
+    //message to be sent in the mail body
+    public static final String MESSAGE = "This message is to say HEY!!!!";
+}
